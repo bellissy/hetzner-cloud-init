c2ebbc13df58d11ef0940280d5615202b03ec8bb
Bellissy IT Team <77968934+bellissy-it@users.noreply.github.com>
2021-03-01 11:22:49 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -50,18 +50,7 @@ AUTH_PW=$AUTH_PASSWORD
 REPOS=\$(curl -s -u \$AUTH_USER:\$AUTH_PW https://\$REGISTRY_URL/v2/_catalog | jq -r .repositories[])
 
 for REPO in \$REPOS; do
-
-    LATEST_SHA=\$(curl -s -u \$AUTH_USER:\$AUTH_PW https://\$REGISTRY_URL/v2/\$REPO/manifests/latest -H 'Accept: application/vnd.docker.distribution.manifest.v2+json' | jq .config.digest)
-
-    TAGS=\$(curl -s -u \$AUTH_USER:\$AUTH_PW  https://\$REGISTRY_URL/v2/\$REPO/tags/list | jq -r ' .tags | map(select(. != "latest")) | .[]')
-
-    for TAG in \$TAGS ; do
-        SHA=\$(curl -s -u \$AUTH_USER:\$AUTH_PW https://\$REGISTRY_URL/v2/\$REPO/manifests/\$TAG -H 'Accept: application/vnd.docker.distribution.manifest.v2+json' | jq .config.digest)
-        if [ "\$SHA" == "\$LATEST_SHA" ]; then
-            echo "kubectl --insecure-skip-tls-verify set image deployments/\$REPO \$REPO=\$REGISTRY_URL/\$REPO:\$TAG"
-            kubectl --insecure-skip-tls-verify set image deployments/\$REPO \$REPO=\$REGISTRY_URL/\$REPO:\$TAG
-        fi
-    done
+    docker pull --all-tags \$REGISTRY_URL/\$REPO
 done
 EOF
 
